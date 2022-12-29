1b98cb5c2e4a539b69d430d716e234642a29c069
Memenashi <shunsuke.vrml@icloud.com>
2022-12-29 21:23:27 +0900

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,41 @@
-const path = require("path");
+import { join, resolve as _resolve } from "path";
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "main.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: { presets: ["@babel/preset-env", "@babel/react"] },
-          },
-          {
-            loader: "ts-loader",
-            options: {
-              configFile: path.resolve(__dirname, "tsconfig.json"),
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    port: 3000,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-  },
-  target: "web",
+export const mode = "development";
+export const entry = "./src/index.tsx";
+export const output = {
+  path: join(__dirname, "public"),
+  filename: "main.js",
 };
+export const module = {
+  rules: [
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/react"] },
+        },
+        {
+          loader: "ts-loader",
+          options: {
+            configFile: _resolve(__dirname, "tsconfig.json"),
+          },
+        },
+      ],
+    },
+    {
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+    },
+  ],
+};
+export const devServer = {
+  static: {
+    directory: join(__dirname, "public"),
+  },
+  port: 3000,
+};
+export const resolve = {
+  extensions: [".ts", ".tsx", ".js", ".json"],
+};
+export const target = "web";
